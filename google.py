ca73a0b5edf8d57c08dcc64b292062b42bc7b6c9
alterock2 <alterock2@gmail.com>
2023-01-18 10:16:36 +0300

    сохранение в гугл таблицу, добавление цвета в нее и xls файл

--- google.py
+++ google.py
@@ -12,7 +12,7 @@ wks = sh[1]
 
 df['Запчасть'] = df['Запчасть'].fillna('Ждем')
 
-wks.set_dataframe(df, (1, 1))
+#wks.set_dataframe(df, (1, 1))
 
 color = {
 #"blue": 1,
@@ -21,15 +21,16 @@ color = {
   "alpha": 1
 }
 
-wks2 = sh[1]
+wks2 = sh[0]
 
-wks2.add_conditional_formatting('N', 'N', 'NUMBER_GREATER_THAN_EQ', {'backgroundColor':{'red': 0.8}}, ['30'])
+wks2.add_conditional_formatting('N', 'N', 'NUMBER_GREATER_THAN_EQ', {'backgroundColor':{'red': 0.8},
+                                'textFormat': {'bold': True}}, ['30'])
 
 wks2.add_conditional_formatting('N', 'N', 'NUMBER_BETWEEN', {'backgroundColor':
-{"red": 2, "green": 23, "alpha": 1}}, ['14', '30'])
+{"red": 250/255, "green": 77/255, "blue": 77/255, "alpha": 1}}, ['14', '30'])
 
 wks2.add_conditional_formatting('N', 'N', 'NUMBER_LESS_THAN_EQ', {'backgroundColor':
-{"green": 1, "alpha": 1}}, ['14'])
+{"red": 255/255, "green": 158/255, "blue": 158/255, "alpha": 1}}, ['14'])
 
 wks2.adjust_column_width(start=1, end=1, pixel_size=30)
 #read = wks.get_as_df()
@@ -41,3 +42,10 @@ wks2.add_conditional_formatting('K', 'K', 'TEXT_EQ', {'backgroundColor':color}, 
 header = wks2.cell('A1')
 header.set_text_format('bold', True)
 header.update()
+
+#for i, row in df.iterrows():
+#  if row['Товар'] == 'Отпариватель':
+#    if row['Статус'] == 'Получено':
+#      print("yes of course")
+#    else:
+#      print('no')
