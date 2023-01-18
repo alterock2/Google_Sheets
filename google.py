b2d42b6816d809ac23041226e53d3b78b43316b9
alterock2 <alterock2@gmail.com>
2023-01-18 16:05:07 +0300

    better version, new borders

--- google.py
+++ google.py
@@ -2,6 +2,8 @@ import pandas as pd
 import pygsheets
 import openpyxl
 
+from pygsheets import DataRange
+
 gc = pygsheets.authorize(service_file=r"C:\Users\user\Desktop\stable-woods-374912-6149e61555af.json")
 
 df = pd.read_excel(r"C:\Users\user\Desktop\Сервис.xlsx")
@@ -39,9 +41,18 @@ wks2.adjust_column_width(start=1, end=1, pixel_size=30)
 
 wks2.add_conditional_formatting('K', 'K', 'TEXT_EQ', {'backgroundColor':color}, ['Выдан'])
 
-header = wks2.cell('A1')
-header.set_text_format('bold', True)
-header.update()
+model_cell = wks2.cell('A1')
+model_cell.set_text_format('bold', True)
+model_cell.text_format['fontSize'] = 11
+#model_cell.text_format['wrapStrategy'] = 'WRAP'
+model_cell.color = (0.5, 0.5, 0.5, 1)
+model_cell.wrap_strategy = 'WRAP'
+model_cell.borders ={"top": {'style': 'DOUBLE', "width": 1,"color": {'red':1}}}
+
+
+DataRange('A2','D2', worksheet=wks2).apply_format(model_cell)
+
+DataRange('E', 'E', worksheet=wks2).apply_format((model_cell))
 
 #for i, row in df.iterrows():
 #  if row['Товар'] == 'Отпариватель':
@@ -49,3 +60,5 @@ header.update()
 #      print("yes of course")
 #    else:
 #      print('no')
+
+DataRange('A1', 'L1', worksheet=wks2).update_borders(top=True, right=True, bottom=True, left=True, style='SOLID_THICK', red=0, green=0, blue=0)
